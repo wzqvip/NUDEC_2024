ca5a7f1e4734387cb46b7514ae0f1726763e59fe
Taco <wangziqi2002vip@163.com>
2024-07-31 17:09:00 +0800

    rm -rf

--- 费案/Keil项目_MSP/Hardware/encoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ENCODER_H
-#define _ENCODER_H
-#include "ti_msp_dl_config.h"
-#include "board.h"
-
-#endif
--- 费案/Keil项目_MSP/Hardware/key.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KEY_H
-#define _KEY_H
-#include "ti_msp_dl_config.h"
-#include "board.h"
-
-uint8_t click(void);
-
-#endif
