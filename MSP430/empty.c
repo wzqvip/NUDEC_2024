f9929985e06ece45e05117e2d6f96c79b3502d0f
Taco <wangziqi2002vip@163.com>
2024-07-31 17:56:38 +0800

    bugfix

--- MSP430/empty.c
+++ MSP430/empty.c
@@ -359,7 +359,7 @@ void APP_Show(void)
 	}
 	else
 	{
-		sendToPc();
+		// sendToPc();
 		printf("{A%%d:%d:%d:%d:%f:%f:%f}$", CCD_Zhongzhi, encoderA_cnt, encoderB_cnt, Pitch, Roll, Yaw); // 打印到APP上面 显示波形
 	}
 }
@@ -432,143 +432,143 @@ void Get_Angle(uint8_t way)
 
 
 
-/******************************************************************************
-***
-* FUNCTION NAME: void sendToPc(void) *
-* CREATE DATE : 20170707 *
-* CREATED BY : XJU *
-* FUNCTION : 将待发送的信息通过串口发送至上位机*
-* MODIFY DATE : NONE *
-* INPUT : void *
-* OUTPUT : NONE *
-* RETURN : NONE *
-*******************************************************************************
-**/
-void sendToPc(void)
-{
-	int i;
-	slove_data();
-	printf("*");
-	printf("LD");
-	for (i = 2; i < 134; i++)
-	{
-		printf("%c", binToHex_high(SciBuf[i])); // 以字符形式发送高4位对应的16进制
-		printf("%c", binToHex_low(SciBuf[i]));	// 以字符形式发送低?位对应的16进制
-	}
-	printf("00"); // 通信协议要求
-	printf("#");  // 通信协议要求
-}
+// /******************************************************************************
+// ***
+// * FUNCTION NAME: void sendToPc(void) *
+// * CREATE DATE : 20170707 *
+// * CREATED BY : XJU *
+// * FUNCTION : 将待发送的信息通过串口发送至上位机*
+// * MODIFY DATE : NONE *
+// * INPUT : void *
+// * OUTPUT : NONE *
+// * RETURN : NONE *
+// *******************************************************************************
+// **/
+// void sendToPc(void)
+// {
+// 	int i;
+// 	slove_data();
+// 	printf("*");
+// 	printf("LD");
+// 	for (i = 2; i < 134; i++)
+// 	{
+// 		printf("%c", binToHex_high(SciBuf[i])); // 以字符形式发送高4位对应的16进制
+// 		printf("%c", binToHex_low(SciBuf[i]));	// 以字符形式发送低?位对应的16进制
+// 	}
+// 	printf("00"); // 通信协议要求
+// 	printf("#");  // 通信协议要求
+// }
 
-void slove_data(void)
-{
-	int i;
-	SciBuf[0] = 0;
-	SciBuf[1] = 132;
-	SciBuf[2] = 0;
-	SciBuf[3] = 0;
-	SciBuf[4] = 0;
-	SciBuf[5] = 0;
-	for (i = 0; i < 128; i++)
-		SciBuf[6 + i] = ADV[i];
-}
+// void slove_data(void)
+// {
+// 	int i;
+// 	SciBuf[0] = 0;
+// 	SciBuf[1] = 132;
+// 	SciBuf[2] = 0;
+// 	SciBuf[3] = 0;
+// 	SciBuf[4] = 0;
+// 	SciBuf[5] = 0;
+// 	for (i = 0; i < 128; i++)
+// 		SciBuf[6 + i] = ADV[i];
+// }
 
-/******************************************************************************
- ***
- * FUNCTION NAME: binToHex_low(u8 num) *
- * CREATE DATE : 20170707 *
- * CREATED BY : XJU *
- * FUNCTION : 将二进制低8位转换16进制*
- * MODIFY DATE : NONE *
- * INPUT : u8 *
- * OUTPUT : NONE *
- * RETURN : char *
- *******************************************************************************
- **/
-char binToHex_low(uint8_t num)
-{
-	uint8_t low_four;
-	low_four = num & 0x0f;
-	if (low_four == 0)
-		return ('0');
-	else if (low_four == 1)
-		return ('1');
-	else if (low_four == 2)
-		return ('2');
-	else if (low_four == 3)
-		return ('3');
-	else if (low_four == 4)
-		return ('4');
-	else if (low_four == 5)
-		return ('5');
-	else if (low_four == 6)
-		return ('6');
-	else if (low_four == 7)
-		return ('7');
-	else if (low_four == 8)
-		return ('8');
-	else if (low_four == 9)
-		return ('9');
-	else if (low_four == 10)
-		return ('A');
-	else if (low_four == 11)
-		return ('B');
-	else if (low_four == 12)
-		return ('C');
-	else if (low_four == 13)
-		return ('D');
-	else if (low_four == 14)
-		return ('E');
-	else if (low_four == 15)
-		return ('F');
-}
+// /******************************************************************************
+//  ***
+//  * FUNCTION NAME: binToHex_low(u8 num) *
+//  * CREATE DATE : 20170707 *
+//  * CREATED BY : XJU *
+//  * FUNCTION : 将二进制低8位转换16进制*
+//  * MODIFY DATE : NONE *
+//  * INPUT : u8 *
+//  * OUTPUT : NONE *
+//  * RETURN : char *
+//  *******************************************************************************
+//  **/
+// char binToHex_low(uint8_t num)
+// {
+// 	uint8_t low_four;
+// 	low_four = num & 0x0f;
+// 	if (low_four == 0)
+// 		return ('0');
+// 	else if (low_four == 1)
+// 		return ('1');
+// 	else if (low_four == 2)
+// 		return ('2');
+// 	else if (low_four == 3)
+// 		return ('3');
+// 	else if (low_four == 4)
+// 		return ('4');
+// 	else if (low_four == 5)
+// 		return ('5');
+// 	else if (low_four == 6)
+// 		return ('6');
+// 	else if (low_four == 7)
+// 		return ('7');
+// 	else if (low_four == 8)
+// 		return ('8');
+// 	else if (low_four == 9)
+// 		return ('9');
+// 	else if (low_four == 10)
+// 		return ('A');
+// 	else if (low_four == 11)
+// 		return ('B');
+// 	else if (low_four == 12)
+// 		return ('C');
+// 	else if (low_four == 13)
+// 		return ('D');
+// 	else if (low_four == 14)
+// 		return ('E');
+// 	else if (low_four == 15)
+// 		return ('F');
+// }
 
-/******************************************************************************
-***
-* FUNCTION NAME: binToHex_low(u8 num) *
-* CREATE DATE : 20170707 *
-* CREATED BY : XJU *
-* FUNCTION : 将二进制高8位转换16进制*
-* MODIFY DATE : NONE *
-* INPUT : u8 *
-* OUTPUT : NONE *
-* RETURN : char *
-*******************************************************************************
-**/
-char binToHex_high(uint8_t num)
-{
-	uint8_t high_four;
-	high_four = (num >> 4) & 0x0f;
-	if (high_four == 0)
-		return ('0');
-	else if (high_four == 1)
-		return ('1');
-	else if (high_four == 2)
-		return ('2');
-	else if (high_four == 3)
-		return ('3');
-	else if (high_four == 4)
-		return ('4');
-	else if (high_four == 5)
-		return ('5');
-	else if (high_four == 6)
-		return ('6');
-	else if (high_four == 7)
-		return ('7');
-	else if (high_four == 8)
-		return ('8');
-	else if (high_four == 9)
-		return ('9');
-	else if (high_four == 10)
-		return ('A');
-	else if (high_four == 11)
-		return ('B');
-	else if (high_four == 12)
-		return ('C');
-	else if (high_four == 13)
-		return ('D');
-	else if (high_four == 14)
-		return ('E');
-	else if (high_four == 15)
-		return ('F');
-}
+// /******************************************************************************
+// ***
+// * FUNCTION NAME: binToHex_low(u8 num) *
+// * CREATE DATE : 20170707 *
+// * CREATED BY : XJU *
+// * FUNCTION : 将二进制高8位转换16进制*
+// * MODIFY DATE : NONE *
+// * INPUT : u8 *
+// * OUTPUT : NONE *
+// * RETURN : char *
+// *******************************************************************************
+// **/
+// char binToHex_high(uint8_t num)
+// {
+// 	uint8_t high_four;
+// 	high_four = (num >> 4) & 0x0f;
+// 	if (high_four == 0)
+// 		return ('0');
+// 	else if (high_four == 1)
+// 		return ('1');
+// 	else if (high_four == 2)
+// 		return ('2');
+// 	else if (high_four == 3)
+// 		return ('3');
+// 	else if (high_four == 4)
+// 		return ('4');
+// 	else if (high_four == 5)
+// 		return ('5');
+// 	else if (high_four == 6)
+// 		return ('6');
+// 	else if (high_four == 7)
+// 		return ('7');
+// 	else if (high_four == 8)
+// 		return ('8');
+// 	else if (high_four == 9)
+// 		return ('9');
+// 	else if (high_four == 10)
+// 		return ('A');
+// 	else if (high_four == 11)
+// 		return ('B');
+// 	else if (high_four == 12)
+// 		return ('C');
+// 	else if (high_four == 13)
+// 		return ('D');
+// 	else if (high_four == 14)
+// 		return ('E');
+// 	else if (high_four == 15)
+// 		return ('F');
+// }
 
